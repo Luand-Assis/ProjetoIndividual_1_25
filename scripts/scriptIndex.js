e89cbfb90c6c91c5fb66323f679a440360f9a5aa
Luand-Assis <luan.assis@sptech.school>
2025-05-02 13:52:47 -0300

    Conectei tela index com cadastro e login

---
+++ scripts/scriptIndex.js
@@ -0,0 +1,12 @@
+// ----- HEADER -----
+function index() {
+    window.location = 'index.html';
+}
+
+function login() {
+    window.location = 'login.html';
+}
+
+function cadastro() {
+    window.location = 'cadastro.html';
+}
