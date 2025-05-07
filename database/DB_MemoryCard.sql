98610f23112131e1bd0b7aead83b98e2cd0ac865
Luand-Assis <luan.assis@sptech.school>
2025-05-07 17:27:53 -0300

    Banco de Dados adicionado

---
+++ database/DB_MemoryCard.sql
@@ -0,0 +1,35 @@
+create database DB_MemoryCard;
+use DB_MemoryCard;
+
+create table TB_Usuarios (
+idUsuario int primary key auto_increment,
+nickname varchar(12) not null unique,
+corPinguim tinyint not null,
+email varchar(45) not null unique,
+-- (criar cronstraint check?)
+senha varchar(45) not null
+);
+
+create table TB_Partidas (
+idPartida int auto_increment,
+idUsuario int,
+	constraint fkPartidaUsuario
+		foreign key (idUsuario) references TB_Usuarios(idUsuario),
+    constraint pkPartidaUsuario
+		primary key (idPartida, idUsuario),
+qtd_erros int,
+tempo_inicio datetime default current_timestamp not null,
+tempo_final datetime
+);
+
+create table TB_Estatisticas (
+idEstatistica int auto_increment,
+idUsuario int,
+	constraint fkEstatisticaUsuario
+		foreign key (idUsuario) references TB_Usuarios(idUsuario),
+    constraint pkEstatisticaUsuario
+		primary key (idEstatistica, idUsuario),
+media_tempo int,
+media_erros int,
+qtd_partidas int
+);
