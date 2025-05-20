3b9159862ebb4516bde6d745bc786439a23f4640
Luand-Assis <luan.assis@sptech.school>
2025-05-19 23:24:28 -0300

    Sendo implementado sistema de achievements

--- src/database/DB_MemoryCard.sql
+++ src/database/DB_MemoryCard.sql
@@ -9,7 +9,6 @@ email varchar(45) not null unique,
 	constraint chkEmail check(email like '%@%'),
 senha varchar(45) not null
 );
-select * from TB_Usuarios;
 
 create table TB_Partidas (
 idPartida int auto_increment,
@@ -18,18 +17,26 @@ idUsuario int,
 		foreign key (idUsuario) references TB_Usuarios(idUsuario),
     constraint pkPartidaUsuario
 		primary key (idPartida, idUsuario),
-qtd_erros int,
-tempo_segundos int
+qtd_erros int not null,
+tempo_segundos int not null
 );
 
-create table TB_Estatisticas (
-idEstatistica int auto_increment,
-idUsuario int,
-	constraint fkEstatisticaUsuario
-		foreign key (idUsuario) references TB_Usuarios(idUsuario),
-    constraint pkEstatisticaUsuario
-		primary key (idEstatistica, idUsuario),
-media_tempo int not null,
-media_erros int not null,
-qtd_partidas int not null
+create table TB_Conquistas (
+idConquista int primary key auto_increment,
+nomeConquista varchar(45) not null unique,
+descricao varchar(255) not null
 );
+
+create table TB_UsuarioConquista (
+idConquista int,
+idUsuario int,
+	constraint fkConquista foreign key (idConquista) references TB_Conquistas(idConquista),
+    constraint fkUsuario foreign key (idUsuario) references TB_Usuarios(idUsuario),
+    primary key (idConquista, idUsuario),
+dataHora datetime default current_timestamp
+);
+
+insert into TB_Conquistas values
+	(default, 'Primeira Vitória', 'Concluiu o Memory Card Game do Club Penguin.'),
+	(default, 'Memória Perfeita', 'Concluiu o Memory Card Game sem erros.'),
+	(default, 'Rápido como um Pinguim', 'Concluiu o Memory Card Game em menos de 15 segundos.');
